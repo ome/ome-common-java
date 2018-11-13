6811bacec4a0cb90f83da6489d4f14b004141b3c
Simon Li <spli@dundee.ac.uk>
2018-11-13 15:02:36 +0000

    Create 2MB file for testing

--- location-server/start-location.sh
+++ location-server/start-location.sh
@@ -18,12 +18,17 @@ sleep 2;
 
 ./mc config host add ome-common-java-minio-test http://localhost:$PORT ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
 
+set +x
+
 for SUFFIX in public private; do
     ./mc ls ome-common-java-minio-test/bioformats.test.$SUFFIX || \
         ./mc mb ome-common-java-minio-test/bioformats.test.$SUFFIX
     ./mc ls ome-common-java-minio-test/bioformats.test.$SUFFIX/single-channel.ome.tiff || \
         curl -sfS https://downloads.openmicroscopy.org/images/OME-TIFF/2016-06/bioformats-artificial/single-channel.ome.tiff | \
             ./mc pipe ome-common-java-minio-test/bioformats.test.$SUFFIX/single-channel.ome.tiff
+    # 2MB file for testing seek
+    for n in `seq 65536`; do printf '.% 30d\n' $n; done | \
+        ./mc pipe ome-common-java-minio-test/bioformats.test.$SUFFIX/2MBfile.txt
 done
 
 ./mc policy public ome-common-java-minio-test/bioformats.test.public
